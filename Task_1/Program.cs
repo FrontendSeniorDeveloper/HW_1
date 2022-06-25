ab57cde3b432195c2029ab4fa2ffc374a64023d0
KirillBoldinov <kirillboldinov@gmail.com>
2022-06-25 12:28:09 +0300

    Task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+    Console.WriteLine("Максимальное число: " + number_1);
+    Console.WriteLine("Минимальное число : " + number_2);
+}
+
+else 
+{
+    Console.WriteLine("Максимальное число: " + number_2);
+    Console.WriteLine("Минимальное число: " + number_1);
+}
