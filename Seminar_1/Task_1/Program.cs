e6248005a49bed8f65f30b183f102786c43bb1e5
KirillBoldinov <kirillboldinov@gmail.com>
2022-06-29 08:56:21 +0300

    Seminar_1

---
+++ Seminar_1/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+    Console.WriteLine("Максимальное число: " + number_1);
+    Console.WriteLine("Минимальное число : " + number_2);
+}
+
+else 
+{
+    Console.WriteLine("Максимальное число: " + number_2);
+    Console.WriteLine("Минимальное число: " + number_1);
+}
