c8021784b5cada84c09a6607cfd4eb8a0d0b164b
KirillBoldinov <kirillboldinov@gmail.com>
2022-06-29 09:29:40 +0300

    Task 1

--- Seminar_2/Task_1/Program.cs
+++ Seminar_2/Task_1/Program.cs
@@ -8,7 +8,7 @@ int number = new Random().Next(100, 1000);
 Console.WriteLine(number);
 
 int secondNumber = number / 10; 
-int result = secondNumber % 10;
+int result = secondNumber % 10; 
 
 Console.WriteLine(result);
  
