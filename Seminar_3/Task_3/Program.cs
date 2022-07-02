ef8b4ceba6c746a5c71e3bbd9e93276d95473447
KirillBoldinov <kirillboldinov@gmail.com>
2022-07-02 17:26:28 +0300

    Seminar 3

--- Seminar_3/Task_3/Program.cs
+++ Seminar_3/Task_3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("введите число");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"Куб числа {i} = {i*i*i}");
+}
+
+
