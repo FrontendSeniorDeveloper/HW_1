e9653bdde568218959f9cdaf5520273f00085413
KirillBoldinov <kirillboldinov@gmail.com>
2022-07-10 16:18:05 +0300

    Seminar 5

---
+++ Seminar_5/Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+int[] array = new int[10];
+
+int size = array.Length;
+int numberОfEvenNumbers = 0;
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0)
+    {
+        numberОfEvenNumbers += 1;
+    }
+}
+
+Console.WriteLine($"Исходный массив: {String.Join("; ", array)}");
+Console.WriteLine($"Количество четных чисел в массиве = {numberОfEvenNumbers}");
---
+++ Seminar_5/Task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 38: Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+double[] array = new double[7];
+
+double size = array.Length;
+double maximumArrayElement = 0;
+double minimumArrayElement = 100;
+double differenceMaximumAndMinimum = 0;
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().NextDouble();
+    if (array[i] < minimumArrayElement)
+    {
+        minimumArrayElement = array[i];
+    }
+    if (array[i] > maximumArrayElement)
+    {
+        maximumArrayElement = array[i];
+    }
+
+    differenceMaximumAndMinimum = maximumArrayElement - minimumArrayElement;
+}
+
+Console.WriteLine($"Массив вещественных чисел: {String.Join("; ", array)}");
+Console.WriteLine(
+    $"Разница между максимальным и минимальным элементом массива = {differenceMaximumAndMinimum}"
+);
