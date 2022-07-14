0079e600252bdc631c3f609f777fab246601a788
KirillBoldinov <kirillboldinov@gmail.com>
2022-07-14 09:49:05 +0300

    Seminar 6

---
+++ Seminar_6/Task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write($"Введите количество чисел: ");
+int startNumber = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[startNumber];
+int size = array.Length;
+int resultPositive = 0;
+int index = 0;
+
+while (index < size)
+{
+    Console.Write($"Введите {index + 1} число: ");
+    array[index] = Convert.ToInt32(Console.ReadLine());
+    index++;
+}
+
+Console.WriteLine($"[{String.Join("; ", array)}]");
+
+for (int i = 0; i < size; i++)
+
+    if (array[i] > 0)
+    {
+        resultPositive += 1;
+    }
+
+Console.WriteLine($"Количество чисел больше нуля = {resultPositive}");
---
+++ Seminar_6/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу,
+// которая найдёт точку пересечения двух прямых,
+// заданных уравнениями
+// y = k1 * x + b1,
+// y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите первое чиcло : ");
+double firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе чиcло : ");
+double secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье чиcло : ");
+double thirdNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите четвертое чиcло : ");
+double fourthNumber = Convert.ToInt32(Console.ReadLine());
+// y = y ->
+// k1*x+b1 = k2*x+b2 ->
+// k1*x-k2*x = b2-b1 ->
+// (k1-k2)*x = b2-b1 -> 
+// x = (b2-b1) / (k1-k2)
+double firstСoordinate = (thirdNumber - firstNumber) / (secondNumber - fourthNumber);
+double secondCoordinate = secondNumber * firstСoordinate + firstNumber;
+
+Console.WriteLine($"Точка пересечения координат: ({firstСoordinate}; {secondCoordinate})");
