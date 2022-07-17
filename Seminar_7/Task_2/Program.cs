214c218123a190d3ab1390851d73962728384d11
KirillBoldinov <kirillboldinov@gmail.com>
2022-07-17 10:45:21 +0300

    Seminar 7

---
+++ Seminar_7/Task_2/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 50. Напишите программу,
+которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание,
+что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+Console.Write("Введи количество строчек: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи число: ");
+int startNumber = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+
+bool checkingElement = false;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(1, 11);
+    }
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (startNumber == matrix[i, j])
+        {
+            Console.WriteLine($"Значение элемента {startNumber} = matrix[{i},{j}]");
+            checkingElement = true;
+        }
+    }
+}
+
+if (checkingElement == false)
+{
+    Console.WriteLine($"Такого злемента  нет");
+}
---
+++ Seminar_7/Task_3/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+Console.WriteLine("Введи количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    
+    Console.WriteLine();
+}
+
+Console.Write("Среднее арифметическое в каждом столбце: ");
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sumElements = 0;
+    double arithmeticMean = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sumElements = (matrix[i, j] + sumElements);
+        arithmeticMean = sumElements / matrix.GetLength(0);
+    }
+
+    Console.Write(arithmeticMean + "; ");
+}
